52190b770d96c1b33a53f30cf4d8b0ea112f2824
llamicron <llamicron@gmail.com>
2021-02-24 20:49:55 -0600

    Adds Proj1, hello world

---
+++ Proj1/add32.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+extern int32_t addS32(int32_t x, int32_t y);
+extern uint32_t addU32(uint32_t x, uint32_t y);
+
+int main() {
+    int32_t a, b, c;
+    a = 100;
+    b = -200;
+    c = addS32(a, b);
+    printf("a + b = %d + %d = %d\n", a, b, c);
+
+    uint32_t d, e, f;
+    d = 100;
+    e = -200;
+    f = addU32(d, e);
+    printf("d + e = %d + %d = %d\n", d, e, f);
+    
+    return EXIT_SUCCESS;
+}
