e61695f4d3f20600d772bb190cf18075531b5278
llamicron <llamicron@gmail.com>
2021-04-23 13:56:32 -0500

    Adds L18 code

---
+++ L18/float.c
@@ -0,0 +1,48 @@
+// April 8, 2021
+// Lecture 18
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define COUNT 9
+
+void uint32ToBinaryString(char str[], uint32_t x) {
+    uint32_t index;
+    uint32_t mask = 0x400000;
+    for (index = 0; index < 23; index++) {
+        if (x & mask)
+            str[index] = '1';
+        else
+            str[index] = '0';
+        mask >>= 1;
+    }
+
+    str[23] = '\0';
+}
+
+// This will show the S, E, F values of encoded floating point numbers
+int main() {
+    int i;
+    uint32_t value;
+    uint32_t s;
+    uint32_t e;
+    uint32_t f;
+    char str[24];
+
+    float x[COUNT] = {1, -1, 2, -2, -0.640625, 0.3333333333, 1024, 1024 + 1/8192.0, 1024 + 1/16384.0};
+
+    printf("Float Value\t\t\tHex\t\tS\tE\tF\n");
+    for (i = 0; i < COUNT; i++)
+    {
+        value = *((uint32_t*) &x[i]);
+        s = value >> 31;
+        e = (value >> 23) & 0xFF;
+        f = value & 0x7FFFFF;
+        uint32ToBinaryString(str, f);
+        printf("%20.10f\t\t0x%08x\t%1d\t%3d\t%s\n", x[i], value, s, e, str);
+    }
+
+    return EXIT_SUCCESS;
+
+}
+
